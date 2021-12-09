b71b9c83279c891d4f558e6a9a4c08096c34d8ff
hnguyen418 <hnguyen418@VN-PF2MFBWT>
2021-12-09 08:39:43 +0700

    Add project files.

---
+++ DoAnCTDL/BSTree.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+
+namespace DoAnCTDL
+{
+	public class BSTree
+	{
+		private int size;
+		public Node root;
+
+		public BSTree()
+		{
+			size = 0;
+			root = null;
+		}
+
+		public int Size()
+		{
+			return size;
+		}
+//Insert
+		public  void insert(Node newNode)
+		{
+			root = insert(root, newNode);
+		}
+
+		private Node insert(Node node, Node newNode)
+		{
+			if (node == null)
+			{
+				size++;
+				return newNode;
+			}
+
+			if (newNode.lessThanOrEqual(node.product))
+			{
+				node.left = insert(node.left, newNode);
+			}
+			else
+			{
+				node.right = insert(node.right, newNode);
+			}
+			return node;
+		}
+//Search
+		public Node search(int productId)
+		{
+			return search(this.root, productId);
+		}
+
+		private Node search(Node node, int productId)
+		{
+			if (node == null)	return null;
+
+
+			if (node.hasproduct(productId))
+			{
+				return node;
+			}
+			if (node.lessThan(productId))
+			{
+				return search(node.right, productId);
+			}
+			else
+			{
+				return search(node.left, productId);
+			}
+		}
+//Delete
+		public void delete(int productId)
+		{
+			root = delete(root, productId);
+		}
+
+		public Node delete(Node node, int productId)
+		{
+			if (node == null) return null;
+
+
+			if (node.hasproduct(productId))
+			{
+				size--;
+				if (node.left == null)
+				{
+					return node.right;
+				}
+				Node maxNode = node.left;
+				Node preNode = maxNode;
+				while (maxNode.right != null)
+				{
+					preNode = maxNode;
+					maxNode = maxNode.right;
+				}
+
+				//Console.Write("\n\npreNode ");
+				//preNode.printData();
+				//Console.Write("\n\nmaxnode ");
+				//maxNode.printData();
+
+				maxNode.right = node.right;
+				if (maxNode != node.left)
+				{
+					preNode.right = maxNode.left;
+					maxNode.left = node.left;
+					
+				}
+				return maxNode;
+			}
+			if (node.lessThan(productId))
+			{
+				node.right = delete(node.right, productId);
+			}
+			else
+			{
+				node.left = delete(node.left, productId);
+			}
+			return node;
+		}
+//TraversePreOrder
+		public void TraversePreOrder()
+		{
+			Console.Write("\n\nTraversePreOrder ");
+			TraversePreOrder(root);
+		}
+
+		private void TraversePreOrder(Node node)
+		{
+			if (node == null) return;
+
+			node.Data();
+			TraversePreOrder(node.left);
+			TraversePreOrder(node.right);
+		}
+//TraverseInOrder
+		public void TraversetInOrder()
+		{
+			Console.Write("\n\nTraverseInOrder ");
+			TraverseInOrder(root);
+		}
+
+		private void TraverseInOrder(Node node)
+		{
+			if (node == null) return;
+
+
+			TraverseInOrder(node.left);
+			node.Data();
+			TraverseInOrder(node.right);
+		}
+//TraversePostOrder
+		public void TraversePostOrder()
+		{
+			Console.Write("\n\nTraversePostOrder ");
+			TraversePostOrder(root);
+		}
+
+		private void TraversePostOrder(Node node)
+		{
+			if (node == null)
+			{
+				return;
+			}
+
+			TraversePostOrder(node.left);
+			TraversePostOrder(node.right);
+			node.Data();
+		}
+	}
+}
